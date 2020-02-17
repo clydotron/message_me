0c4f6dd2e5a0a14decbc08bcfedc97ea117162a6
Brian Green <bgreen007@cox.net>
2020-02-17 15:20:48 -0600

    add messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Joey", password:"password")
+User.create(username: "Taco", password:"password")
+User.create(username: "Taz", password:"password")
